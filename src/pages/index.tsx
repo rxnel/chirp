cda3957321f41fe2f72da7386ef29877a192692a
Ronel Aguilar <ronel.aguilar@icloud.com>
2023-10-11 11:24:46 -0400

    using nextjs Image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,7 @@ import { RouterOutputs, api } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
 
 dayjs.extend(relativeTime);
 const CreatePostWizard = () => {
@@ -15,10 +16,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      <img
+      <Image
         src={user.imageUrl}
         alt="Profile image"
         className="h-14 w-14 rounded-full"
+        height={56}
+        width={56}
       />
       <input
         placeholder="Type some emojis"
@@ -33,7 +36,13 @@ const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full" />
+      <Image
+        alt={`@${author.username}'s profile picture`}
+        src={author.profileImageUrl}
+        height={56}
+        width={56}
+        className="h-14 w-14 rounded-full"
+      />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
