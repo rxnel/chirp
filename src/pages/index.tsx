bd8038f77c8f6a30c5466aede55d155428f2d6fb
Ronel Aguilar <ronel.aguilar@icloud.com>
2023-10-11 14:50:24 -0400

    adding user validation for posting + posting + refreshing on post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,12 +9,22 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 
 import { LoadingPage } from "~/components/loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
+
 const CreatePostWizard = () => {
+  const [input, setInput] = useState("");
+  const ctx = api.useContext();
+
   const { user } = useUser();
-  console.log(user);
   if (!user) return null;
+  const { mutate, isLoading: isPosting } = api.posts.create.useMutation({
+    onSuccess: () => {
+      setInput("");
+      ctx.posts.getAll.invalidate();
+    },
+  });
 
   return (
     <div className="flex w-full gap-3">
@@ -28,7 +38,11 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis"
         className="grow bg-transparent outline-none"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        disabled={isPosting}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
@@ -57,6 +71,7 @@ const PostView = (props: PostWithUser) => {
     </div>
   );
 };
+
 const Feed = () => {
   const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
